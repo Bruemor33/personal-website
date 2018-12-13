620914c83da80c17b527d01fe6024851266c2bb5
Bruemor <arymer1@gmail.com>
2018-12-13 11:56:50 -0500

    Created a homepage baseline react component

---
+++ public/views/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Welcome = () => {
+    return (
+        <div className="main-heading">
+            <h1>Andrew Rymer</h1>
+        </div>
+    )
+}
+
+module.exports = Welcome;
