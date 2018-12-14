ae9dcc14613c99a0c9862862130398f85ea89413
Bruemor <arymer1@gmail.com>
2018-12-14 11:56:15 -0500

    Renamed the home.js file for better naming conventions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Welcome} from '../public/views/home.js';
+import {MainHeading} from '../public/views/main-heading.js';
+import {About} from '../public/views/about.js';
+import {Skills} from '../public/views/skills.js';
 import './index.scss';
 
-const Home = () => {
+const Landing = () => {
     return (
-        <Welcome />
+        <div id="landing">
+            <MainHeading />
+            <About />
+            <Skills />
+        </div>
     )
 }
 
-ReactDOM.render(<Home /> , document.getElementById('index'));
+ReactDOM.render(<Landing /> , document.getElementById('index'));
